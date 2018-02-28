b652e538a4df2122c6a7481063f3a2220189914a
Unknown <fadasgo@gmail.com>
2018-02-28 12:20:21 -0400

    main actualizado

--- Operativos/Proyecto I/lectura2.c
+++ Operativos/Proyecto I/lectura2.c
@@ -1,15 +1,20 @@
 #include<stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
+// Lectura Proyecto I
 
-// Lectura Proyecto II
+typedef struct ruta{
+   char name[200]; // Nombre del Producto
+}Paths;
+// Funciones
 
 int countlines(char *filename){
     FILE *fp;
     int count = 0;  // contador de lineas
     char c;  // To store a character read from file
 
-    // Open the file
+    // Abrimos el archivo
     fp = fopen(filename, "r");
 
     // chequea si el archivo existe
@@ -31,14 +36,15 @@ int countlines(char *filename){
     return count;
 };
 
-char* readd(int numLines, char *filename){
-  char* arrayP = malloc(numLines * sizeof(char));
-  char* InicioArray = arrayP;
+Paths* readd(int numLines, char *filename){
+  Paths* arrayP = malloc(numLines * sizeof(Paths));
+  Paths* InicioArray = arrayP;
   char* line = NULL;
   //char *line2 = NULL;
   size_t len = 0;
   ssize_t readd;
   char *token;
+  Paths ruta;
 
   FILE *file = fopen ( filename, "r" );
 
@@ -47,29 +53,59 @@ char* readd(int numLines, char *filename){
   }
 
 	while ((readd = getline(&line, &len, file)) != -1) {
-		//printf("Retrieved line of length %zu :\n", readd);
-		//printf("%s", line);
-    //line2 = line;
     char* dirs;
     token = strtok(line,"\n");
-    printf("%s \n", token);
-    arrayP = token;
+    //printf("%s \n", token);
+    strcpy(ruta.name,token);
+    *arrayP = ruta;
+    //printf("%s \n", arrayP->name);
     arrayP++;
   }
+
   free(line);
   fclose(file);
   return InicioArray;
 }
 
 int main(int argc, char *argv[]) {
-  char* arrayDirs;
-  char* copiaArrayDirs; // Copia de la direccion en donde comienza la lista de productos
-  int lines;
-  // lines contiene el numero de lineas en el archivo
+
+  // primero buscamos los directorios hojas para luego buscar las rutas de los archivos
+  long size;
+  char *buf;
+  char *ptr;
+
+  // Buscamos la direccion del path donde estamos situados para despues poder buscar el archivo.txt creado mas adelante
+  size = pathconf(".", _PC_PATH_MAX);
+  if ((buf = (char *)malloc((size_t)size)) != NULL);
+  ptr = getcwd(buf, (size_t)size); // poner condicional para saber si la ruta que vamos a usar es la de donde estamos parados o la dada por el flag
+
+  //printf("%s\n",ptr ); // print para verificar el path
+
+  char *comand = "find /home/fadasgo/Escritorio/Operativos -type d -links 2  | tee archivo.txt";
+  system(comand);
+  system("clear");
+
+  // ahora procedemos a leer el archivo creado para buscar los archivos de los directorios hojas
+
+  Paths* arrayDirs;
+  Paths* copiaArrayDirs; // Copia de la direccion en donde comienza la lista de productos
+  char* path[200];
+  int lines;   // lines contiene el numero de lineas en el archivo
+
   lines = countlines(argv[1]);
-  printf("LINES: %d\n",lines);
-  printf("Lineas \n");
+  //printf("LINES: %d\n",lines);
+  //printf("Lineas \n");
   // Recibimos la direccion donde comienza el arreglo
   arrayDirs = readd(lines,argv[1]);
+  copiaArrayDirs = arrayDirs; // creamos copia del inicio del arreglo dinamico
+
+  //printf("PATH i: %s\n",copiaArrayDirs->name);
+
+  for (int i = 0; i < lines; i++) {
+    printf("PATH i: %s\n", arrayDirs->name);
+    //strcpy(*path,arrayDirs->name);
+    arrayDirs++;
+  }
+
   return 0;
 }

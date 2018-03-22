8022177eec39ae50a9917ef60f593cdb5d4ed7c5
jcellomarcano <12-10359@usb.ve>
2018-03-22 15:06:20 -0400

    blablabal

--- Operativos/Proyecto 2 Sistemas Operativos/shell/buscarArch.sh
+++ Operativos/Proyecto 2 Sistemas Operativos/shell/buscarArch.sh
@@ -1,14 +1,21 @@
-
+#!/bin/bash
 if [ $# -ne 1 ]
 then
     echo "Usage - $0  file-name"
     exit 1
 fi
 
-if [ -f $1 ]
+if [ -d $1 ]
 then
-    echo "$1 file exist"
+    echo "el directorio: $1   exist"
+
+elif [ -f $1 ]
+then
+  echo "el archivo: $1   existe"
+
 else
     echo "Sorry, $1 file does not exist"
 fi
-    
+
+ATRIBUTOS_SCRIPT=`find $1 ~/Operativos/`
+echo -e "$ATRIBUTOS_SCRIPT \n"

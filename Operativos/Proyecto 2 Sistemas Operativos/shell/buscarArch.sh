cbe92e0c40bc1db79c6b69dd89ba4d04d6afea16
jcellomarcano <12-10359@usb.ve>
2018-03-22 20:32:29 -0400

    esto es un commit

--- Operativos/Proyecto 2 Sistemas Operativos/shell/buscarArch.sh
+++ Operativos/Proyecto 2 Sistemas Operativos/shell/buscarArch.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-if [ $# -ne 1 ]
+if [ $# -ne 1 ] #revisa si ha sido ingresado algun dato a buscar
 then
     echo "Usage - $0  file-name"
     exit 1
 fi
-
-if [ -d $1 ]
+#$1 es la variable que contiene la palabra ingresada
+if [ -d $1 ] #aqui se encarga de revisar si es directorio
 then
-    echo "el directorio: $1   exist"
+    echo "el directorio: $1 exist"
 
-elif [ -f $1 ]
+elif [ -f $1 ]#aqui si es para verifircar si existe
 then
-  echo "el archivo: $1   existe"
+  echo "el archivo: $1 existe"
 
 else
     echo "Sorry, $1 file does not exist"
 fi
 
-ATRIBUTOS_SCRIPT=`find $1 ~/Operativos/`
-echo -e "$ATRIBUTOS_SCRIPT \n"
+#Busqueda directa a traves
+Busca_Dir=`find $1 ~/Operativos/`
+echo -e "$Busca_Dir \n"

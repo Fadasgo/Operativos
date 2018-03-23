58dd0c554ea8076dd9d7966d0057a41723e0ac80
jcellomarcano <12-10359@usb.ve>
2018-03-23 06:14:28 -0400

    Fino en la busca de direcctorio y archivo al estilo menu

--- Operativos/Proyecto 2 Sistemas Operativos/shell/buscarArch.sh
+++ Operativos/Proyecto 2 Sistemas Operativos/shell/buscarArch.sh
@@ -1,36 +1,39 @@
 #!/bin/bash
-if [ $# -ne 1 ] #revisa si ha sido ingresado algun dato a buscar
-then
-    echo "Usage - $0  file-name"
-    exit 1
-fi
-
-#$1 es la variable que contiene la palabra ingresada
-if [ -d $1 ] #aqui se encarga de revisar si es directorio
-then
-    echo "el directorio: $1 exist"
-
-elif [ -f $1 ] #aqui si es para verifircar si existe
-then
-  echo "el archivo: $1 existe"
-
-else
-    echo "Sorry, $1 file does not exist"
-fi
-
 #Busqueda directa a traves
-echo -e "Ingrese la direccion a buscar \n \nSi desea la Carpeta actual ingrese: . \n"
+echo -e "\nIngrese la direccion COMPLETA a buscar \n"
 #variable para ingresar una direccion a buscar
 read dirToSearch
 
 echo $dirToSearch
 
-Busca_Dir=`find $dirToSearch -type d -name "$1"`
+#variable que indentifica si es directorio o archivo
+echo "Ingrese el tipo de busqueda: Si es archivo f, si es directorio: d "
+read $tipo
 
-if [ $Busca_Dir -z ]
+if [[ $tipo == "d" ]];
 then
-  echo "judete"
-else
-  echo -e "$Busca_Dir \n"
+  Busca_Dir=`find $dirToSearch -type d -name "$1"`
+
+  if [ $Busca_Dir -z ]
+  then
+    echo "la direccion ingresada esta vacia - aborta"
+    exit 1
+  else
+    echo -e "$Busca_Dir \n"
+  fi
+
+elif [[ $tipo == "f" ]];
+then
+  Busca_Dir=`find $dirToSearch -type f -name "$1"`
+
+  if [ $Busca_Dir -z ]
+  then
+    echo "la direccion ingresada esta vacia - aborta"
+    exit 1
+  else
+    echo -e "$Busca_Dir \n"
+  fi
 fi
+
+
 #estructura de verificacion

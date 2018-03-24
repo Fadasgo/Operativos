d6b046cd2dbe415933989b592ca3b0d8fd45ec57
jcellomarcano <12-10359@usb.ve>
2018-03-24 03:33:47 -0400

    here is a commit

--- Operativos/Proyecto 2 Sistemas Operativos/shell/shell/shell.c
+++ Operativos/Proyecto 2 Sistemas Operativos/shell/shell/shell.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main()
+int main(argc,argv)
+char *argv[50];
+int argc;
 {
+    //printf("%s\n",argv[1]);
+    char src[50];
+    char dest[50];
+    strcpy(src, "bash buscarArch.sh ");
+    strcpy(dest, argv[1]);
+    strcat(src,dest);
+    printf("puestooooo : %s\n", src );
     puts("Will execute sh with the following script :");
-    puts("bash buscarArch.sh Tarea");
+    puts(src);
     puts("Starting now:");
-    system("bash buscarArch.sh Tarea");
+    system(src);
     return 0;
 }
